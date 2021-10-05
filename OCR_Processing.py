e94fe7295b5ef041a583f37a20080f99d776d085
Luke Chin A Foeng <lukegtc@aol.com>
2021-10-05 22:58:36 +0200

    Working on getting the contours to merge

--- OCR_Processing.py
+++ OCR_Processing.py
@@ -37,7 +37,7 @@ class Receipt:
 
     '''Assigns pixel black or white depending on which side of the threshold its on. Helps remove noise.'''
     def threshold(self):
-        self.thresholded = cv2.threshold(self.blur,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)[1]
+        self.thresholded = cv2.threshold(self.blur,100,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)[1]
         cv2.imwrite(cwd+'\\'+'threshholded'+'\\'+'threshholded'+self.im_name,self.thresholded)
 
     '''Creates and sizes kernal'''
@@ -55,24 +55,70 @@ class Receipt:
     '''Finds contours and saves them to the contour file'''
     def contour(self,visualize = 0,min_h = 20, min_w = 5 ):
         self.contours = cv2.findContours(self.dilated,cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        self.contours = self.contours[0] if len(self.contours) == 2 else self.contours[0]
+        self.contours = self.contours[0] #if len(self.contours) == 2 else self.contours[0]
         self.contours = sorted(self.contours, key=lambda x: cv2.boundingRect(x)[0])
+        #This is gonna need to be optimized 
+        def contour_combine():
+            contour_set = self.contours
+            cg_set = []
+            new_contours = []
+            for item in contour_set:
+                x,y,w,h = cv2.boundingRect(item)
+                cg_set.append(x+w/2,y+h/2,item)
+            #Go through contour_set
+            #Find closest characters in +- x direction
+            
+            while cg_set != []:
+                closest = []
+                key_cg = cg_set[0]
+                for cg in cg_set[1:]:
 
+                
+                
+                    return
         
+        # Implement this code below but w contours that have cgs close to each other and to the right or left of each other
+        
+        for contour in self.contours:
+            x,y,w,h = cv2.boundingRect(contour)
+            
+        # contours = np.vstack(contours)
+        # print(self.contours)
+        '''combining bounding boxes'''
+        # a and b are boxes
+        def union(a,b):
+            x = min(a[0], b[0])
+            y = min(a[1], b[1])
+            w = max(a[0]+a[2], b[0]+b[2]) - x
+            h = max(a[1]+a[3], b[1]+b[3]) - y
+            return (x, y, w, h)
+
+        def intersection(a,b):
+            x = max(a[0], b[0])
+            y = max(a[1], b[1])
+            w = min(a[0]+a[2], b[0]+b[2]) - x
+            h = min(a[1]+a[3], b[1]+b[3]) - y
+            if w<0 or h<0: return () # or (0,0,0,0) ?
+            return (x, y, w, h)
+        
+        # Uses the above functions to comine the nearest contours
+
         i = 0
         if not os.path.isdir(cwd+'\\'+'ROI_files'+'\\' + self.im_name): 
             os.mkdir(cwd+'\\'+'ROI_files'+'\\' + self.im_name)
         rois = []
         for contour in self.contours:
             x,y,w,h = cv2.boundingRect(contour)
-
+            # hull = cv2.convexHull(contour)
             #Filters boxes
-
+            # concat = np.concatenate(contours)
+            # hulls = cv2.convexHull(concat)
             if h > min_h and w > min_w:
                 roi = self.image[y:y+h,x:x+h]
                 rois.append(roi)
                 if visualize == 1:
                     cv2.rectangle(self.image,(x,y), (x+w,y+h), (0,255,0),2)
+                    # cv2.drawContours(self.image,[hull],0,(0,255,0),2)
                 # cv2.imwrite(cwd+'\\'+'ROI_files'+'\\' + str(self.im_name)[:-4]+'\\'+'roi' + str(i) +self.im_name ,roi)
                 i +=1
         self.rois = rois
@@ -80,26 +126,35 @@ class Receipt:
 
 
     def im2str(self):
+        ocr_out = []
         for item in self.rois:
+            
+            #Black Box that I need to learn more about
             ocr = pytesseract.image_to_string(item)
+            
             ocr = ocr.split('\n')
             for val in ocr:
-                val = val.strip()
-                print(val)
+
+                val = val.strip().replace("\n","")
+                val = val.split(" ")[0]
+                if val!= '\x0c' and len(val) > 0:
+                    ocr_out.append(val)
+            
+        print(ocr_out)   
 
 #TODO: 
-# Label the functions
+# Label the functions (kinda done)
 # Streamline the file management
-#
+#Learn about the Black Box
 
 
 if __name__ == '__main__':
-    im1 = Receipt('receipt.jpg')
+    im1 = Receipt('irl_pic.jpg')
     # print(im1.txt_extract)
     im1.wipe()
     im1.processing()
     im1.threshold()
     im1.kernal()
     im1.dilate()
-    im1.contour()
+    im1.contour(1) #insert a 1 for a visualization in contoured folder
     im1.im2str()
