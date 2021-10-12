ec5e539c9a9b7f031302bed1e7a6c9a13e4182c6
Luke Chin A Foeng <lukegtc@aol.com>
2021-10-12 22:55:36 +0200

    Telegram API kinda sorta working

--- telegram_pics.py
+++ telegram_pics.py
@@ -4,4 +4,19 @@ import matplotlib.pyplot as plt
 import os
 import sys
 import pandas as pd
+import requests
+key = '2068120405:AAGkFNOajfhBe45wq7rgrWrnU4UQe7aAHJM'
+method = "getupdates"
+file_path = ""
+
+cwd = os.getcwd()
+dir = "telegram_pics"
+response = requests.get(f"https://api.telegram.org/bot{key}/{method}")
+
+file_id = response.json()['result'][2]['message']['photo'][0]['file_id']
+file_data = requests.get(f"https://api.telegram.org/bot{key}/getFile?file_id={file_id}")
+print(file_data.json())
+file = file_data.json()['result']['file_path']
+file_download = requests.get(f"https://api.telegram.org/file/bot{key}/{file}")
+
 
